386033fc5b8d41a2d9764c38be20f5d786d27f3c
Gustavo Batista <batistag395@gmail.com>
2022-08-11 12:23:33 -0300

    Primeiro commit do projeto API Catalogo

---
+++ APICatalogo/Migrations/20220810123455_PopulaCategoria.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaCategoria : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("insert into Categorias( ""CategoriaId"", ""Nome"", ""Imagem"")");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
